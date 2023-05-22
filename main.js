94baede14d399d293e89a69bf6bc2404e208b433
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-05-22 12:29:37 +0200

    add class et js

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ function cursor(e) {
     mouseCursor.style.top = e.pageY + "px";
     mouseCursor.style.left = e.pageX + "px";
 }
+
+navLinks.forEach(link => {
+    link.addEventListener("mouseleave", () => {
+        mouseCursor.classList.remove("link-grow");
+    });
+    link.addEventListener("mouseover", () => {
+        mouseCursor.classList.add("link-grow");
+    });
+});
