928454614490e4493a79eb0693d96aaa82d93198
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-05-22 14:19:36 +0200

    final

--- main.js
+++ main.js
@@ -11,8 +11,10 @@ function cursor(e) {
 navLinks.forEach(link => {
     link.addEventListener("mouseleave", () => {
         mouseCursor.classList.remove("link-grow");
+        link.classList.remove("hovered-link");
     });
     link.addEventListener("mouseover", () => {
         mouseCursor.classList.add("link-grow");
+        link.classList.add("hovered-link");
     });
 });
